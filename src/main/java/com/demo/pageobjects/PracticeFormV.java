e62f1286d66f8f27f8e9af6be257f1590c52014b
anshul.kumar <anshul.kumar@IN01L0201000030.ebaotech.com>
2017-03-11 13:14:41 +0530

    Commit locator changes

--- src/main/java/com/demo/pageobjects/PracticeFormV.java
+++ src/main/java/com/demo/pageobjects/PracticeFormV.java
@@ -1,5 +1,32 @@
 package com.demo.pageobjects;
 
-public class PracticeFormV {
+import java.util.List;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PracticeFormV {
+	
+		
+	@FindBy(xpath="/html/body/div/div[5]/div[2]/div/div/form/fieldset/div[5]/strong/label")
+	WebElement ProfilePictureAttach;
+	
+	@FindBy(xpath="/html/body/div/div[5]/div[2]/div/div/form/fieldset/div[5]/input")
+	WebElement BrowseProfilePicture;
+	
+	@FindBy(xpath="/html/body/div/div[5]/div[2]/div/div/form/fieldset/div[7]/strong")
+	WebElement DowloadFramework;
+
+	@FindBy(xpath="/html/body/div/div[5]/div[2]/div/div/form/fieldset/div[8]/a")
+	WebElement LinkHybridFramework;
+	
+	@FindBy(xpath="/html/body/div/div[5]/div[2]/div/div/form/fieldset/div[9]/a")
+	WebElement LinkDownloadTestFile;
+	
+	@FindBy(xpath="/html/body/div/div[5]/div[2]/div/div/form/fieldset/div[11]/strong/label")
+	WebElement LabelAutomationTool;
+	
+	@FindBy(name="tool")
+	List<WebElement> Tools; 
+	
 }
